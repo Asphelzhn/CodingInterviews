c941d0bc7eb8b93c35881cba58f5c68f0b3dfb4a
asphelzhn <568718851@qq.com>
2019-03-23 10:20:40 +0800

    add 04

--- src/Solution04.java
+++ src/Solution04.java
@@ -5,34 +5,7 @@ import java.util.Collections;
 
 public class Solution04 {
     public static void main(String[] args) {
-        ListNode listNode = new ListNode(4);
-        ListNode listNode2 = new ListNode(6);
-        listNode.next = listNode2;
 
-        ArrayList<Integer> array = new Solution04().printListFromTailToHead(listNode);
-        for (Integer i:array) {
-            System.out.println(i+",");
-        }
     }
-    public static class ListNode {
-        int val;
-        ListNode next = null;
 
-        ListNode(int val) {
-            this.val = val;
-        }
-    }
-    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        if(listNode==null){
-            return array;
-        }
-        while (listNode != null){
-            int val = listNode.val;
-            array.add(val);
-            listNode = listNode.next;
-        }
-        Collections.reverse(array);
-        return array;
-    }
 }
