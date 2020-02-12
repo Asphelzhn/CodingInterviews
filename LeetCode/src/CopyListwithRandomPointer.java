76902759a554cdbd14ac5898e731bdd8b74cad1f
asphelzhn <568718851@qq.com>
2020-02-12 23:45:23 +0800

    finish problem Copy List with Random Pointer

---
+++ LeetCode/src/CopyListwithRandomPointer.java
@@ -0,0 +1,9 @@
+/**
+ * Copy List with Random Pointer
+ * 描述
+ * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+ * Return a deep copy of the list.
+ */
+public class CopyListwithRandomPointer {
+    
+}
