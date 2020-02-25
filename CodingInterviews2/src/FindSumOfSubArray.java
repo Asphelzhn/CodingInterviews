63e71ebec47360f2eb22697b523b93edb436f5f1
asphelzhn <568718851@qq.com>
2020-02-25 12:04:41 +0800

    finish problem 43. 从 1 到 n 整数中 1 出现的次数

--- CodingInterviews2/src/FindSumOfSubArray.java
+++ CodingInterviews2/src/FindSumOfSubArray.java
@@ -1,2 +1,18 @@
+/**
+ * 42. 连续子数组的最大和
+ * NowCoder
+ * <p>
+ * 题目描述
+ * {6, -3, -2, 7, -15, 1, 2, 2}，连续子数组的最大和为 8（从第 0 个开始，到第 3 个为止）。
+ */
 public class FindSumOfSubArray {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        int sum = 0;
+        int result_sum = Integer.MIN_VALUE;
+        for (int num : array) {
+            sum = sum < 0 ? num : sum + num;
+            result_sum = Math.max(result_sum, sum);
+        }
+        return result_sum;
+    }
 }
